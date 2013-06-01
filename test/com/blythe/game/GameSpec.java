afcde45c23dad37487dff06b6342034c859feb68
6LYTH3 <ST4R13UCK@gmail.com>
2013-06-01 18:54:18 +0700

    Step:3 Confirm Spec Current category

--- test/com/blythe/game/GameSpec.java
+++ test/com/blythe/game/GameSpec.java
@@ -2,13 +2,49 @@ package com.blythe.game;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameSpec {
+	private Game game;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Before
+	public void before() {
+		game = new Game();
 	}
 
+	@Test
+	public void currentCategoryShouldReturnPOPSpacificPlace() {
+		assertEquals(game.currentCategory(0), "Pop");
+		assertEquals(game.currentCategory(4), "Pop");
+		assertEquals(game.currentCategory(8), "Pop");
+	}
+
+	@Test
+	public void currentCategoryShouldReturnScienceSpacificPlace() {
+		assertEquals(game.currentCategory(1), "Science");
+		assertEquals(game.currentCategory(5), "Science");
+		assertEquals(game.currentCategory(9), "Science");
+	}
+
+	@Test
+	public void currentCategoryShouldReturnSportsSpacificPlace() {
+		assertEquals(game.currentCategory(2), "Sports");
+		assertEquals(game.currentCategory(6), "Sports");
+		assertEquals(game.currentCategory(10), "Sports");
+	}
+
+	@Test
+	public void currentCategoryShouldReturnRockMoreThen10() {
+		assertEquals(game.currentCategory(11), "Rock");
+		assertEquals(game.currentCategory(100), "Rock");
+		assertEquals(game.currentCategory(1000), "Rock");
+	}
+	
+	@Test
+	public void currentCategoryShouldReturnRockLessThen0() {
+		assertEquals(game.currentCategory(-1), "Rock");
+		assertEquals(game.currentCategory(-100), "Rock");
+		assertEquals(game.currentCategory(-1000), "Rock");
+	}
 }
